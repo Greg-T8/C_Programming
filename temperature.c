cf40c085c0868104be1aa1a06b7c22e32c145691
Greg Tate <gregt8@gmail.com>
2023-12-26 06:08:26 -0600

    Added C program for temperature conversion

---
+++ temperature.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table for fahr = 0,20,...,300 */
+
+main()
+{
+    int fahr, celsius;
+    int lower, uppper, step;
+
+    lower = 0;    /* lower limit of temperature table */
+    uppper = 300; /* upper limit */
+    step = 20;    /* step size */
+
+    fahr = lower;
+    while (fahr <= uppper)
+    {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius); // \t is a tab character
+        fahr = fahr + step;
+    }
+}
